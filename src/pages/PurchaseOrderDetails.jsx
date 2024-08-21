723c9a3489c3a6b206308e319ea1803afa3e93bd
SimonMunga <smrampage@gmail.com>
2024-08-21 17:00:27 +0300

    21/8

--- src/pages/PurchaseOrderDetails.jsx
+++ src/pages/PurchaseOrderDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';git 
 import { useParams } from 'react-router-dom';
 import apiService from '../service/apiService'; // Adjust the path as necessary
 import Aside from '../components/Aside';
